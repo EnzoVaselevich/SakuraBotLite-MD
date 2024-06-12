deccd60ffde0de951675ae5b063feca1d443bf76
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-12 14:23:33 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 import { es } from "./lib/idiomas/total-idiomas.js"
 
-global.owner = ['00000000']]
+global.owner = [['00000000']]
 
 global.suittag = []
 global.prems = []
