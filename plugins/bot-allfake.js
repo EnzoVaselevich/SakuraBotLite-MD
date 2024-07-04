5dc176ef3c3a7a65afa1ad10dc497bd8b8f1875d
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-07-04 11:57:10 -0300

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -56,7 +56,7 @@ global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 
 //global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: '🌟 Bienvenido ' + nombre, body: team, sourceUrl: yt, thumbnail: 'https://telegra.ph/file/1861aab98389b13db8588.jpg' }}} //Fake simple
 
-global./fake/ = { contextInfo: { mentionedJid: conn.parseMention(wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: '✰્᭄͜͡ 𝑵𝒂𝒊𝒍𝒐𝒖 𝑩𝒐𝒕 - 𝑴𝑫 🪷' }, externalAdReply: { title: wm, body: `👋 Hola ` + nombre, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: sakuraImg, thumbnail: sakuraImg, sourceUrl: md }}}, { quoted: m } //fake con reenviado de canal
+//global.fake = { contextInfo: { mentionedJid: conn.parseMention(wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: '✰્᭄͜͡ 𝑵𝒂𝒊𝒍𝒐𝒖 𝑩𝒐𝒕 - 𝑴𝑫 🪷' }, externalAdReply: { title: wm, body: `👋 Hola ` + nombre, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: sakuraImg, thumbnail: sakuraImg, sourceUrl: md }}}, { quoted: m } //fake con reenviado de canal
 }
 
 export default handler
